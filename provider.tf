050922be95a722ffaf4afa535d44bbd660c9f256
piyushmj12 <76661538+piyushmj12@users.noreply.github.com>
2023-02-26 11:53:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.56.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
-terraform {
-      required_providers {
-         aws = {
-         source = "hashicorp/aws"
-         version = "= 4.56.0"
-        }
-     }
-  }
